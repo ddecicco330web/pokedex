7c7cc39d18d47a001c78f969fcc5a929a49c9044
ddecicco330web <ddecicco330@gmail.com>
2023-03-21 21:38:34 +0900

    add a pokemon list with pokemon objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,27 @@
-alert('Hello World!');
+let pokemonList= [];
 
-let favoriteFood = 'pizza';
-document.write(favoriteFood);
+let bulbasaur={
+    name: 'Bulbasaur',
+    height: 7,
+    type: ['grass','poison']
+};
+
+let charmander={
+    name: 'Charmander',
+    height: 6,
+    type: ['fire']
+};
+
+let squirtle={
+    name: 'Squirtle',
+    height: 5,
+    type: ['water']
+};
+
+pokemonList[0] = bulbasaur;
+pokemonList[1] = charmander;
+pokemonList[2] = squirtle;
+
+console.log(pokemonList[0]);
+console.log(pokemonList[1]);
+console.log(pokemonList[2]);
